e63b927d6f024bae52a8186afb0b94debd8ae033
Jens Hviid <jens@hviid.ch>
2017-11-06 17:55:19 +0100

    Small correction for the news link

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class AppHeader extends Component {
       <div className="AppHeader">
         <img src={ logo } className="AppLogo" alt="logo" />
         <nav>
-          <Link to='/'>News</Link> &nbsp;
+          <Link to='/news'>News</Link> &nbsp;
           <Link to='/links'>Links</Link> &nbsp;     
           <Link to='/delegates'>Delegates</Link> &nbsp;
           <Link to='/voters'>Voters</Link> &nbsp;
